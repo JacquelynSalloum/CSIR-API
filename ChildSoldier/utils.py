d34e29c1f60efdd79e8f9cc7786d28feb4767075
EricD <eric.desjardins11@gmail.com>
2015-10-20 12:20:34 -0300

    ISSUE-4: Add section serializer test

---
+++ ChildSoldier/utils.py
@@ -0,0 +1,17 @@
+def dict_from_attrs(obj, list_of_attrs):
+   """
+   ys: List of keys to extract
+   :return: New dictionary using the given dict, containing only the given keys
+   :param obj: Object to have attributes extracted
+   :param list_of_attrs: List of attributes to extract
+   :return: Dictionary built by extracting the attributes from the given object
+   """
+   return {attr: getattr(obj, attr) for attr in list_of_attrs}
+
+
+def dict_with_keys(dictionary, list_of_keys):
+   """
+   :param dictionary: Dict to have keys extracted
+   :param list_of_ke
+   """
+   return {key: dictionary[key] for key in list_of_keys}
