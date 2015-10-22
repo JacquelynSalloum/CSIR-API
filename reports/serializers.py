ad1586236b5c715e706985b5000852c3eae77e39
Wilson Chiang <wilsonc91@gmail.com>
2015-10-21 23:12:38 -0300

    ISSUE-4: Change to ModelSerializer

--- reports/serializers.py
+++ reports/serializers.py
@@ -1,17 +1,20 @@
 from reports.models import CountryReport, Maps, Section
 from rest_framework import serializers
 
-class CountryReportSerializer(serializers.HyperlinkedModelSerializer):
+
+class CountryReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = CountryReport
-        fields = ('title', 'subtitle', 'created_at', 'updated_at')
+        fields = ('title', 'subtitle')
 
-class MapsSerializer(serializers.HyperlinkedModelSerializer):
+
+class MapsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Maps
         fields = ('country', 'map_url', 'report')
 
-class SectionSerializer(serializers.HyperlinkedModelSerializer):
+
+class SectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Section
         fields = ('title', 'report', 'order', 'section', 'content')
