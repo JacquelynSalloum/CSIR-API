c0c289d7ce65ea134f400fc068613deabda9c2e2
baderk <baderkanawati@gmail.com>
2015-10-29 11:58:09 -0300

    ISSUE-36

--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from reports.models import Map, Section, CountryReport
 from reports.serializers import MapsSerializer, SectionSerializer, CountryReportSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class CountryReportViewSet(viewsets.ModelViewSet):
@@ -16,3 +19,12 @@ class MapsViewSet(viewsets.ModelViewSet):
 class SectionViewSet(viewsets.ModelViewSet):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
+
+class PostPermission(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
