2997d3d6db15b72b9cf8e9728fec9a9b294b03aa
Wilson Chiang <wilsonc91@gmail.com>
2015-11-03 14:15:57 -0400

    ISSUE-72: List CountryReport simplified

--- reports/views.py
+++ reports/views.py
@@ -1,18 +1,30 @@
 from rest_condition import Or
 from rest_framework import viewsets
+from rest_framework.response import Response
 from ChildSoldier.utils import get_guest_permissions_class
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, get_object_or_404
 from reports.models import Map, Section, CountryReport, MapPoint, Country
 from rest_framework.permissions import IsAuthenticated
 from reports.serializers import (MapSerializer, SectionSerializer, CountryReportSerializer, UserSerializer,
-                                 MapPointSerializer, CountrySerializer)
+                                 MapPointSerializer, CountrySerializer, CountryReportRetrieveSerializer)
 from django.contrib.auth.models import User
 
 
 class CountryReportViewSet(viewsets.ModelViewSet):
     permission_classes = (Or(IsAuthenticated, get_guest_permissions_class(['list', 'retrieve'])), )
-    queryset = CountryReport.objects.all()
     serializer_class = CountryReportSerializer
+    queryset = CountryReport.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        queryset = CountryReport.objects.all()
+        serializer = CountryReportSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        queryset = CountryReport.objects.all()
+        countryreport = get_object_or_404(queryset, pk=pk)
+        serializer = CountryReportRetrieveSerializer(countryreport)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
