34d52973176077f2ca84aef00febcd4e7114c619
EricD <eric.desjardins11@gmail.com>
2015-11-03 11:42:09 -0400

    ISSUE-34 Add user endpoints

--- reports/serializers.py
+++ reports/serializers.py
@@ -1,11 +1,13 @@
 from reports.models import CountryReport, Map, Section
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 class CountryReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = CountryReport
         fields = ('id', 'title', 'subtitle')
+        owner = serializers.ReadOnlyField(source='owner.username')
 
 
 class MapsSerializer(serializers.ModelSerializer):
@@ -18,3 +20,9 @@ class SectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Section
         fields = ('id', 'title', 'report', 'order', 'section', 'content')
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username',)
--- reports/views.py
+++ reports/views.py
@@ -1,12 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from reports.models import Map, Section, CountryReport
 from reports.serializers import MapsSerializer, SectionSerializer, CountryReportSerializer
-
+from django.contrib.auth.models import User
+from serializers import UserSerializer
 
 class CountryReportViewSet(viewsets.ModelViewSet):
     queryset = CountryReport.objects.all()
     serializer_class = CountryReportSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class MapsViewSet(viewsets.ModelViewSet):
     queryset = Map.objects.all()
@@ -16,3 +20,11 @@ class MapsViewSet(viewsets.ModelViewSet):
 class SectionViewSet(viewsets.ModelViewSet):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
+
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
