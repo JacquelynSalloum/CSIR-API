3d07bc4b1aaebbea7a0538120f1f84b5a4203b7b
Wilson Chiang <wchiang@aio-tv.com>
2015-11-27 20:51:31 -0400

    Revert "Microsite initial implementation"

---
+++ Microsite/index.php
@@ -0,0 +1,33 @@
+<?php
+  require("template/header.php");
+ ?>
+
+
+<div class="container">
+   <?php require("template/nav.php") ?>
+   <div class="row">
+     <div class="col-md-12">
+       <div class="panel panel-default">
+         <div class="panel-heading">
+           <h3 class="panel-title">Remeo Dallaire Child Soldier Initiative - Country Reports</h3>
+         </div>
+         <div class="panel-body">
+
+         </div>
+         <div class="panel-footer">
+
+         </div>
+       </div>
+     </div>
+   </div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+
+
+<?php
+  require("template/footer.php");
+ ?>
---
+++ Microsite/template/boilerplate.php
@@ -0,0 +1,19 @@
+<?php
+  require("template/header.php");
+ ?>
+
+
+<div class="container">
+   <?php require("template/nav.php") ?>
+   <?php // TODO: Add content here ?>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+
+
+<?php
+  require("template/footer.php");
+ ?>
