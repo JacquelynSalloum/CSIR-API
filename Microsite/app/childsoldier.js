36b75e72adb2f6c11dbd9e4706c8deb5fad88159
wilson chiang <wilsonc91@gmail.com>
2015-11-27 21:45:47 -0400

    Added sections for reports

--- Microsite/app/childsoldier.js
+++ Microsite/app/childsoldier.js
@@ -14,10 +14,15 @@ angular.module('Eggly', [
             {"id": 2, "title": "Report of USA", "url": "http://alistapart.com/", "country": "USA" },
         ];
 
+        $scope.sections = [
+          {"id": 0, "title": "Section of Syria report", "content": "this is only a test", "report": "Report of Syria" },
+          {"id": 1, "title": "Section of Canada report", "content": "this is only a test", "report": "Report of Canada" },
+          {"id": 2, "title": "Section of USA report", "content": "this is only a test", "report": "Report of USA" },
+        ]
+
         $scope.isCreating = false;
         $scope.isEditing = false;
         $scope.currentCountry = null;
-        $scope.editedReport = null;
 
         function isCurrentCountry(country) {
             return $scope.currentCountry !== null && country.name === $scope.currentCountry.name;
@@ -25,15 +30,22 @@ angular.module('Eggly', [
 
         function setCurrentCountry(country) {
             $scope.currentCountry = country;
+            $scope.currentReport = null;
         }
-
+      
         $scope.isCurrentCountry = isCurrentCountry;
         $scope.setCurrentCountry = setCurrentCountry;
 
-        function isSelectedReport(reportId) {
-            return $scope.editedReport !== null && $scope.editedReport.id === reportId;
+      $scope.currentReport = null;
+      function isCurrentReport(report) {
+          return $scope.currentReport !== null && report.title === $scope.currentReport.title;
+        }
+  
+        function setCurrentReport(report) {
+          $scope.currentReport = report;
         }
 
-        $scope.isSelectedReport = isSelectedReport;
+        $scope.isCurrentReport = isCurrentReport;
+        $scope.setCurrentReport = setCurrentReport;
     })
 ;
