a965381816a1ed5bda92cea4c2e34f2e709eb4b5
Wilson Chiang <wilsonc91@gmail.com>
2015-11-29 18:28:08 -0400

    Various Microsite Changes - Use ID instead of name for retrieving data - Support nested sections - Add general hiding of content when switching pages - Couple small bug fixes

--- Microsite/app/childsoldier.js
+++ Microsite/app/childsoldier.js
@@ -1,51 +1,57 @@
-angular.module('Eggly', [
+angular.module('ChildSoldier', [
 
-])
-    .controller('MainCtrl', function ($scope) {
-        $scope.countries = [
-            {"id": 0, "name": "Syria"},
-            {"id": 1, "name": "Canada"},
-            {"id": 2, "name": "USA"},
-        ];
+    ])
 
-        $scope.reports = [
-            {"id": 0, "title": "Report of Syria", "url": "http://angularjs.org", "country": "Syria" },
-            {"id": 1, "title": "Report of Canada", "url": "http://angularjs.org", "country": "Canada" },
-            {"id": 2, "title": "Report of USA", "url": "http://alistapart.com/", "country": "USA" },
-        ];
+    .controller('MainCtrl', function ($scope, $http) {
 
-        $scope.sections = [
-          {"id": 0, "title": "Section of Syria report", "content": "this is only a test", "report": "Report of Syria" },
-          {"id": 1, "title": "Section of Canada report", "content": "this is only a test", "report": "Report of Canada" },
-          {"id": 2, "title": "Section of USA report", "content": "this is only a test", "report": "Report of USA" },
-        ]
+        $http.get('http://127.0.0.1:8000/countries/').success(function(data) {
+            $scope.countries = data;
+        });
+
+        $http.get('http://127.0.0.1:8000/countryreports/').success(function(data) {
+            $scope.reports = data;
+        });
+
+        $http.get('http://127.0.0.1:8000/sections/').success(function(data) {
+            $scope.sections = data;
+        });
 
         $scope.isCreating = false;
         $scope.isEditing = false;
         $scope.currentCountry = null;
 
         function isCurrentCountry(country) {
-            return $scope.currentCountry !== null && country.name === $scope.currentCountry.name;
+            return $scope.currentCountry !== null && country.id === $scope.currentCountry.id;
         }
 
         function setCurrentCountry(country) {
             $scope.currentCountry = country;
             $scope.currentReport = null;
         }
-      
+
         $scope.isCurrentCountry = isCurrentCountry;
         $scope.setCurrentCountry = setCurrentCountry;
 
-      $scope.currentReport = null;
-      function isCurrentReport(report) {
-          return $scope.currentReport !== null && report.title === $scope.currentReport.title;
+        $scope.currentReport = null;
+        function isCurrentReport(report) {
+            return $scope.currentReport !== null && report.id === $scope.currentReport.id;
         }
-  
+
         function setCurrentReport(report) {
-          $scope.currentReport = report;
+            $scope.currentReport = report;
         }
 
         $scope.isCurrentReport = isCurrentReport;
         $scope.setCurrentReport = setCurrentReport;
+
+        $scope.openSections = [];
+        function selectSection(section) {
+            if ($scope.openSections.indexOf(section.id) == -1) {
+                $scope.openSections.push(section.id);
+            } else {
+                $scope.openSections.pop(section.id);
+            }
+        }
+        $scope.selectSection = selectSection;
     })
 ;
