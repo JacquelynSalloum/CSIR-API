bbe55a6ea97e33356bf604e217cff4927942ab7f
Wilson Chiang <wilsonc91@gmail.com>
2015-12-08 12:18:16 -0400

    ISSUE-106: Install  django-cors-headers

--- ChildSoldier/settings.py
+++ ChildSoldier/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = (
     'rest_framework_swagger',
     'reports',
     'rest_framework',
+    'corsheaders'
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -58,6 +60,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'ChildSoldier.urls'
 
